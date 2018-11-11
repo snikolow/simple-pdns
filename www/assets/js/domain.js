747a57d78db0eea74f7871d5062fe38b21e047fc
snikolow <contact@devzone.eu>
2018-11-11 15:52:55 +0200

    Remove dataTable options from the JS file and use data-* attributes instead.

--- www/assets/js/domain.js
+++ www/assets/js/domain.js
@@ -22,17 +22,28 @@ class Domain {
 
         $target.dataTable(
             {
-                searching: false,
-                lengthChange: false,
-                ordering: false,
-                processing: true,
-                serverSide: true,
-                pageLength: 10,
-                ajax: $target.data('url'),
                 columns: [
                     {'data': 'id'},
-                    {'data': 'name'}
-                ]
+                    {'data': 'name'},
+                    {
+                        data: null,
+                        className: 'center',
+                        render: function(data) {
+                            return `
+                                <button class="btn btn-sm btn-default">
+                                    <i class="fa fa-pencil"></i>
+                                    Edit
+                                </button>
+                            `;
+                        }
+                    }
+                ],
+                drawCallback: function(settings) {
+                    // Wrap the table in table-responsive class after the ajax request has been made.
+                    // A bug in the Bootstrap responsive table causes the container to create a horizontal
+                    // scroll after the page. Adding "width 100%" to the table solves the problem for now.
+                    $(document).find('table[data-role="domain-data-table"]').wrap('<div class="table-responsive"></div>');
+                }
             }
         );
     }
